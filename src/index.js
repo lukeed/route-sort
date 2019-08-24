3d142cf4407a37031fd408401bac12e56c444f25
Luke Edwards <luke.edwards05@gmail.com>
2019-08-24 11:48:21 -0700

    chore: golf & perf

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 function toValue(str) {
-	if (str === '*') return ''; // wild
-	if (/^[:](.*)\?/.test(str)) return 2; // param optional
-	if (/^[:](.*)\./.test(str)) return 3; // param w/ suffix
-	if (/^[:]/.test(str)) return 4; // param
+	if (str == '*') return ''; // wild
+	if (/^\:(.*)\?/.test(str)) return 2; // param optional
+	if (/^\:(.*)\./.test(str)) return 3; // param w/ suffix
+	if (/^\:/.test(str)) return 4; // param
 	return 5; // static
 }
 
-function toRank(str, idx) {
-	var i=0, out='', arr = str.split('/');
+function toRank(str) {
+	var i=0, out='', arr=str.split('/');
 	for (; i < arr.length; i++) out += toValue(arr[i]);
-	return { uri: str, index: idx, rank: (+out / arr.length) };
+	return +out / i;
 }
 
-export default function (arr) {
-	return arr.map(toRank).sort(function (a, b) {
-		return b.rank - a.rank || (b.index - a.index);
+export default function (arr, cache) {
+	cache = {};
+	return arr.sort(function (a, b) {
+		return (cache[b] = cache[b] || toRank(b)) - (cache[a] = cache[a] || toRank(a));
 	});
 }
