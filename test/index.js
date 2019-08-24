5c9e3addda00e514c6286e70b4f4a3acd4c224a9
Luke Edwards <luke.edwards05@gmail.com>
2019-08-24 11:33:32 -0700

    chore: fix pathing

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const test = require('tape');
-const rsort = require('../dist/route-sort');
+const rsort = require('../dist/rsort');
 
 test('exports', t => {
 	t.is(typeof rsort, 'function', 'exports a function');
