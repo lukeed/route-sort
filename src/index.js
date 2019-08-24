5dd808924f28813a974f84538744d0a6d7eea998
Luke Edwards <luke.edwards05@gmail.com>
2019-08-24 13:25:09 -0700

    fix: work w/ mix types

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 function toValue(str) {
-	if (str == '*') return ''; // wild
-	if (/^\:(.*)\?/.test(str)) return 2; // param optional
-	if (/^\:(.*)\./.test(str)) return 3; // param w/ suffix
-	if (/^\:/.test(str)) return 4; // param
-	return 5; // static
+	if (str == '*') return 1e11; // wild
+	if (/^\:(.*)\?/.test(str)) return 1111; // param optional
+	if (/^\:(.*)\./.test(str)) return 11; // param w/ suffix
+	if (/^\:/.test(str)) return 111; // param
+	return 1; // static
 }
 
 function toRank(str) {
 	var i=0, out='', arr=str.split('/');
 	for (; i < arr.length; i++) out += toValue(arr[i]);
-	return +out / i;
+	return (i - 1)/+out;
 }
 
 export default function (arr, cache) {
