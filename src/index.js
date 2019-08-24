401aa5d2939ca553720093a81ecc8d2e0f714d1f
Luke Edwards <luke.edwards05@gmail.com>
2019-08-24 11:35:26 -0700

    feat: module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export default function () {
-	//
+function toValue(str) {
+	if (str === '*') return ''; // wild
+	if (/^[:](.*)\?/.test(str)) return 2; // param optional
+	if (/^[:](.*)\./.test(str)) return 3; // param w/ suffix
+	if (/^[:]/.test(str)) return 4; // param
+	return 5; // static
+}
+
+function toRank(str, idx) {
+	var i=0, out='', arr = str.split('/');
+	for (; i < arr.length; i++) out += toValue(arr[i]);
+	return { uri: str, index: idx, rank: (+out / arr.length) };
+}
+
+export default function (arr) {
+	return arr.map(toRank).sort(function (a, b) {
+		return b.rank - a.rank || (b.index - a.index);
+	});
 }
